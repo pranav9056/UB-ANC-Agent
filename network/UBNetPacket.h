478a7fef6037a791995995b0f6196587e4b78649
Jalil Modares <jmodares@gmail.com>
2017-01-02 15:13:44 -0500

    Update UBNetPacket.h

--- network/UBNetPacket.h
+++ network/UBNetPacket.h
@@ -12,19 +12,19 @@ public:
 signals:
 
 public slots:
-    void setSrcID(quint32 srcID) {m_srcID = srcID;}
-    void setDesID(quint32 desID) {m_desID = desID;}
+    void setSrcID(quint8 srcID) {m_srcID = srcID;}
+    void setDesID(quint8 desID) {m_desID = desID;}
     void setPayload(const QByteArray& payload) {m_payload = payload;}
 
 private:
-    quint32 m_srcID;
-    quint32 m_desID;
+    quint8 m_srcID;
+    quint8 m_desID;
 
     QByteArray m_payload;
 
 public:
-    quint32 getSrcID(void) {return m_srcID;}
-    quint32 getDesID(void) {return m_desID;}
+    quint8 getSrcID(void) {return m_srcID;}
+    quint8 getDesID(void) {return m_desID;}
     QByteArray getPayload(void) {return m_payload;}
 
     QByteArray packetize(void);
